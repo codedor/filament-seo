2a8ee96a8c208fefc7e4720fec5d820cccf8ddf0
Jyrki De Neve <jyrki.deneve@gmail.com>
2023-12-07 20:41:50 +0200

    Add contents method to the SeoBuilder facade docblocks

--- src/Facades/SeoBuilder.php
+++ src/Facades/SeoBuilder.php
@@ -2,12 +2,14 @@
 
 namespace Codedor\Seo\Facades;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static void tag(\Codedor\Seo\Tags\Tag $item)
  * @method static void tags(array $items, bool $overwrite = true)
  * @method static string render()
+ * @method static Collection contents()
  * @method static object getTags()
  * @method static string getTag(string $tagName)
  *
