33046e8c5543497d1ad0d8363d147d24fac4c81d
Jyrki De Neve <jyrki.deneve@gmail.com>
2024-10-04 15:45:00 +0300

    Fix phpstan error

--- src/Formats/OgImage.php
+++ src/Formats/OgImage.php
@@ -5,6 +5,7 @@ namespace Codedor\Seo\Formats;
 use Codedor\MediaLibrary\Formats\Format;
 use Codedor\MediaLibrary\Formats\Manipulations;
 use Codedor\Seo\Models\SeoRoute;
+use Spatie\Image\Drivers\ImageDriver;
 use Spatie\Image\Enums\Fit;
 
 class OgImage extends Format
@@ -13,7 +14,7 @@ class OgImage extends Format
 
     protected string $description = 'Format used to display the image for SEO purposes';
 
-    public function definition(): Manipulations
+    public function definition(): Manipulations|ImageDriver
     {
         return $this->manipulations->fit(Fit::Contain, 1200, 630);
     }
